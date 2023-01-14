8851fb5145c5ffc82d299bb7cf5afa85d50ad4df
Lyndon Panton <LyndonPanton@gmail.com>
2023-01-14 03:55:25 +0000

    Created the menu with validation

--- Exercise10/Exercise10/Program.cs
+++ Exercise10/Exercise10/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                int option;
+                Console.WriteLine("****************");
+                Console.WriteLine("Menu: ");
+                Console.WriteLine("1 - New Game");
+                Console.WriteLine("2 - Load Game");
+                Console.WriteLine("3 - Options");
+                Console.WriteLine("4 - Quit");
+                Console.WriteLine("****************");
+
+                while (true)
+                {
+                    Console.WriteLine();
+                    Console.Write("What option would you like to choose (1 - 4): ");
+                    option = Convert.ToInt32(Console.ReadLine());
+
+                    if (option >= 1 && option <= 4)
+                    {
+                        break;
+                    }
+
+                    Console.WriteLine("Error: Invalid option entered");
+                }
+            }
         }
     }
 }
